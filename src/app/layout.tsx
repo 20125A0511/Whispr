e0a99005949467056ab215bad4a2d3f7972b9eab
20125A0511 <your.email@example.com>
2025-05-14 00:57:54 -0400

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthProvider";
 import { ChatProvider } from "@/context/ChatProvider";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </ChatProvider>
         </AuthProvider>
+        <Analytics />
       </body>
     </html>
   );
